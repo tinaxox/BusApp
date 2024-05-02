64d97f05ad29c130816cbf1ad480f5420c71926d
tinaxox <ralevictina02@gmail.com>
2024-05-02 23:55:38 +0200

    added stations list, list of buses on the stations and info of buses

--- gaskghelper/api/routes/index.ts
+++ gaskghelper/api/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { getStationsFromFile } from "../src/get_from_file";
+import { Station } from "../src/types";
 var fs = require("fs");
 
 const router: Router = Router();
@@ -20,15 +21,50 @@ async function getStationStops(station: string) {
   return stations;
 }
 
+function convertToDate(time: string) {
+  console.log(time);
+  var now = new Date();
+  var nowDateTime = now.toISOString();
+  var nowDate = nowDateTime.split("T")[0];
+  var target = new Date(nowDate + "T" + time);
+  return target;
+}
+
+function addMinutes(date: Date, minutes: number) {
+  return new Date(date.getTime() + minutes * 60000);
+}
 // async function loadStations() {
 // const stationsNames = (await getStationsFromFile()) as string[];
-router.get("/:name", async (req, res) => {
+router.get("/:name/:time", async (req, res) => {
   const stationName = req.params.name; //naziv stanice
+  const time = convertToDate(req.params.time);
   console.log(stationName);
   // res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5501");\
   const station_info = await getStationStops(stationName);
-  console.log(station_info);
-  res.send(station_info);
+  console.log(time);
+  const future = addMinutes(time, 10);
+  const past = addMinutes(time, -10);
+  const newDepartureA: Station[] = [];
+  const newDepartureB: Station[] = [];
+  station_info.busDepartureA.map((station: any) => {
+    const dateTime = convertToDate(station.time);
+    if (dateTime >= past && dateTime <= future) {
+      newDepartureA.push(station);
+    }
+    return station;
+  });
+  console.log(future, past);
+  station_info.busDepartureB.map((station: any) => {
+    const dateTime = convertToDate(station.time);
+    if (dateTime >= past && dateTime <= future) {
+      newDepartureB.push(station);
+    }
+    return station;
+  });
+  console.log("ALOOO", newDepartureA, newDepartureB);
+  const obj = { busDepartureA: newDepartureA, busDepartureB: newDepartureB };
+  console.log(obj);
+  res.send(obj);
 });
 // }
 // loadStations();
